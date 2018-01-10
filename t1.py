967e65a5fbe549f48c514808f364c14d44407325
aishwaryapothula <aishwarya.pothula@gmail.com>
2018-01-10 23:22:02 +0530

    wav file

--- t1.py
+++ t1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
+from scipy.io.wavfile import write
 import pickle
+import sounddevice as sd
 n=44100*random.randint(5,31)
 a=np.zeros((n,2))
 for i in range(n):
@@ -10,3 +12,8 @@ print(a)
 pickle_out=open("a.pkl","wb")
 pickle.dump(a,pickle_out)
 pickle_out.close()
+scale=np.int16(a/np.max(np.abs(a)) * 32767)
+write('a.wav',44100,scale)
+
+
+
